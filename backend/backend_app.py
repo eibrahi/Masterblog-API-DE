174ce02f10b1e2945693ad1ae65d9c43a5133df4
eibrahi <newewi@gmail.com>
2026-09-12 12:09:56 +0200

    Implement swagger documentation

--- backend/backend_app.py
+++ backend/backend_app.py
@@ -1,5 +1,6 @@
 from flask import Flask, jsonify, request
 from flask_cors import CORS
+from flask_swagger_ui import get_swaggerui_blueprint
 
 app = Flask(__name__)
 CORS(app)  # This will enable CORS for all routes
@@ -8,12 +9,6 @@ POSTS = [
     {"id": 1, "title": "First post", "content": "This is the first post."},
     {"id": 2, "title": "Second post", "content": "This is the second post."},
 ]
-
-
-@app.route('/api/posts', methods=['GET'])
-def get_posts():
-    return jsonify(POSTS)
-
 
 @app.route('/api/posts', methods=['POST'])
 def add_post():
@@ -32,7 +27,6 @@ def add_post():
         return jsonify(new_post), 201
 
     return jsonify({"error": "Title or Content not found."}), 400
-
 
 @app.route('/api/posts/<int:id>', methods=['DELETE'])
 def delete_post(id):
@@ -69,6 +63,35 @@ def search_posts():
             results.append(post)
     return jsonify(results), 200
 
+@app.route('/api/posts', methods=['GET'])
+def get_sorted_posts():
+    sort = request.args.get('sort')
+    direction = request.args.get('direction')
+    if sort and direction:
+        if sort not in ["title", "content"]:
+            return jsonify({"error": "Sort not found."}), 404
+
+        if direction not in ["asc", "desc"]:
+            return jsonify({"error": "Direction not found."}), 404
+
+        reverse = direction == "desc"
+
+        return jsonify(
+            sorted(POSTS, key=lambda x: x[sort], reverse=reverse))
+
+    return jsonify(POSTS)
+
+SWAGGER_URL="/api/docs"  # (1) swagger endpoint e.g. HTTP://localhost:5002/api/docs
+API_URL="/static/masterblog.json" # (2) ensure you create this dir and file
+
+swagger_ui_blueprint = get_swaggerui_blueprint(
+    SWAGGER_URL,
+    API_URL,
+    config={
+        'app_name': 'Masterblog-API-DE' # (3) You can change this if you like
+    }
+)
+app.register_blueprint(swagger_ui_blueprint, url_prefix=SWAGGER_URL)
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=5002, debug=True)
